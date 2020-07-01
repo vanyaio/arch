5c22ea4f122f7849d1e677441413449e03748c37
Vanya <you@example.com>
2020-07-01 19:37:46 +0300

    save t1

--- t1.c
+++ t1.c
@@ -1,5 +1,6 @@
 #include "mpi.h"
 #include "stdio.h"
+#include "unistd.h"
 
 int main(int argc, char **argv)
 {
@@ -9,6 +10,22 @@ int main(int argc, char **argv)
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 	printf("rank is %d, size is %d\n", rank, size);
+	sleep(1);
 
+	int msg;
+	if (!rank)
+	{
+		msg = 1337;
+		for (int id = 1; id < size; id++) 
+			MPI_Send(&msg, 1, MPI_INT, id, 0, MPI_COMM_WORLD);
+	}
+	else
+	{
+		MPI_Status stat;
+		MPI_Recv(&msg, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
+		printf("rank %d got message: %d\n", rank, msg);
+	}
+
+	MPI_Finalize();
 	return 0;
 }
