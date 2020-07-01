66567a8cecbb005f41656f6870056d81a0edd66a
Vanya <you@example.com>
2020-07-01 20:15:21 +0300

    al

--- t1.c
+++ t1.c
@@ -9,20 +9,60 @@ int main(int argc, char **argv)
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+	if (!rank)
+		printf("info\n");
+	sleep(0.5);
 	printf("rank is %d, size is %d\n", rank, size);
-	sleep(1);
 
-	int msg;
+	sleep(0.5);
+
+	int msg = 1337;
+	int src = (rank + 1) % size;
+	MPI_Status stat;
 	if (!rank)
 	{
-		msg = 1337;
+		printf("circle\n");
+		MPI_Send(&msg, 1, MPI_INT, src, 0, MPI_COMM_WORLD);
+		printf("rank %d send message to rank %d\n", rank, src);
+		MPI_Recv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
+		printf("rank %d recv message from rank %d\n", rank, size - 1);
+	}
+	else
+	{
+		sleep(0.5);
+		MPI_Recv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
+		printf("rank %d recv message from rank %d\n", rank, rank - 1);
+		MPI_Send(&msg, 1, MPI_INT, src, 0, MPI_COMM_WORLD);
+		printf("rank %d send message to rank %d\n", rank, src);
+	}
+
+
+	sleep(0.5);
+	if (!rank)
+	{
+		printf("master->slave\n");
 		for (int id = 1; id < size; id++) 
 			MPI_Send(&msg, 1, MPI_INT, id, 0, MPI_COMM_WORLD);
 	}
 	else
 	{
-		MPI_Status stat;
 		MPI_Recv(&msg, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
+		printf("rank %d got message: %d\n", rank, msg);
+	}
+
+
+	sleep(3);
+	if (!rank)
+		printf("\n\n\neach->each\n");
+
+	for (int id = 0; id < size; id++)
+	{
+		if (id != rank)
+			MPI_Send(&msg, 1, MPI_INT, id, 0, MPI_COMM_WORLD);
+	}
+	for (int i = 0; i < size - 1; i++)
+	{
+		MPI_Recv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
 		printf("rank %d got message: %d\n", rank, msg);
 	}
 
